7fef3f1b66bdadf458f2d0df75922ee91db9d218
Mario Perez <emeyerre@gmail.com>
2022-03-30 21:32:48 +0200

    Called API for data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from "../services/api-service.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  imagesArray: any;
+  datesArray: any;
+
+  constructor(private apiService: ApiServiceService) {
+    this.imagesArray = [];
+    this.datesArray = [
+      '2022-03-29', '2022-03-28', '2022-03-27',
+      '2022-03-26', '2022-03-25', '2022-03-24'
+    ];
+  }
 
   ngOnInit(): void {
+    for (let i = 0, len = this.datesArray.length; i < len; i++) {
+      this.apiService.getImage(this.datesArray[i]).subscribe(data => {
+        this.imagesArray.push(data);
+      });
+    }
   }
 
 }

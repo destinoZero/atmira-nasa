14d284c186c151b663f1b74c63a80e5bff6ee6ec
Mario Perez <emeyerre@gmail.com>
2022-03-31 00:02:10 +0200

    Defined routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from "../app/dashboard/dashboard.component";
+import { ImageDetailComponent } from "../app/image-detail/image-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'image/:date', component: ImageDetailComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
